b853af7ba05c072ef2e7ced9c6d54c1625409e2b
NMR <niumaoru@163.com>
2019-11-08 04:54:48 +0000

    用户微博列表

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -3,20 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Faker;
 
 class TestsController extends Controller
 {
     public function test(){
-        //$this->cube();
-
-        $arr1 = ['a', 'b'];
-        $arr2 = array_map("strtoupper", $arr1);
-        print_r($arr2);
+        $user_ids = ['1','2','3','4'];
+        $faker = app(Faker\Generator::class);
+        echo $faker->randomElement($user_ids);
     }
-
-    function cube($n)
-    {
-        return($n * $n * $n);
-    }
-
 }
