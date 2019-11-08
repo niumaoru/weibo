77984e1652ff5602863e281fa0d0933b0989ca32
NMR <niumaoru@163.com>
2019-11-08 07:21:04 +0000

    删除微博

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
     <a href="https://learnku.com/laravel/courses" target=_blank>
-        刻意练习，每日精进
+        刻意练习，每日精进!
     </a>
 
     <div class="float-right">
